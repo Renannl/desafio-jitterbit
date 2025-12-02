29a814b2cb576466dd22a882726a7e8c7be519de
Renan <mxgames02@gmail.com>
2025-12-02 00:07:28 -0300

    swagger documentation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const orderRoutes = require("./routes/orderRoutes");
 const authRoutes = require("./routes/authRoutes");
+const setupSwagger = require("./swagger"); // importando Swagger
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.use(bodyParser.json());
 // Rotas da API
 app.use("/", authRoutes); // rota de login
 app.use("/", orderRoutes); // rotas de pedidos
+
+// Swagger
+setupSwagger(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`API rodando em http://localhost:${port}`));
